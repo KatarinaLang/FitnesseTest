8e49950b3351da6ba444d5f408ba5c529e1fe1a1
KatarinaLang <42998967+KatarinaLang@users.noreply.github.com>
2019-12-11 16:10:49 +0100

    First classes for calculation

---
+++ RechnenFitNesse/RechnenPlus/src/Rechner.java
@@ -0,0 +1,25 @@
+
+public class Rechner {
+	private int a,b;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int addiere() {
+		return a+b;
+	}
+
+}
